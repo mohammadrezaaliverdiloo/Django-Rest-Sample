b1fa0c6d6df7d268e7a9bf0972ec8ec6485b0609
mohammadreza aliverdiloo <mohammadrezaaliverdiloo@gmail.com>
2024-04-08 21:23:19 +0330

    change docker compose

--- web/web/settings.py
+++ web/web/settings.py
@@ -75,11 +75,20 @@ WSGI_APPLICATION = 'web.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db", # set in docker-compose.yml
+        "PORT": 5432, # default postgres port
 }
+}
+
 
 
 # Password validation
