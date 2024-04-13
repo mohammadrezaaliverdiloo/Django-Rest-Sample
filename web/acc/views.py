c816db68859e09213dc3405ac9a9f8f154dcff36
mohammadreza aliverdiloo <mohammadrezaaliverdiloo@gmail.com>
2024-04-13 22:28:26 +0330

    after signup

--- web/acc/views.py
+++ web/acc/views.py
@@ -1,5 +1,13 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,CreateView
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+    
+    
+class Signup(CreateView):
+    form_class= CustomUserCreationForm
+    success_url= reverse_lazy("login")
+    template_name= "registration/signup.html"
--- web/web/urls.py
+++ web/web/urls.py
@@ -5,5 +5,8 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("articles.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("acc.urls")),
     path('p/',include("pages.urls"))
+    
 ]
